d039beac88bedf147c2414f07ebd6a17f7db4e31
anastasiia <anastasiia.kryvorucho.ri.2026@lpnu.ua>
2026-09-23 23:31:37 +0300

    third commit: z1 and z2

--- lab02/Lab_02.cpp
+++ lab02/Lab_02.cpp
@@ -12,18 +12,18 @@ using namespace std;
 int main() {
 
 	double x;   // вхідний параметр
-	//double z1;  //результат обчислення першого виразу
+	double z1;  //результат обчислення першого виразу
 	double z2;  // результат обчислення другого виразу
 
 	cout << " x = ";
 	cin >> x;
 
-	//z1 = (pow(x, 2) + 2 * x - 3 + (x + 1) * sqrt(pow(x, 2) - 9)) / (pow(x, 2) - 2 * x - 3 + (x - 1) * sqrt(pow(x, 2) - 9));
+	z1 = (pow(x, 2) + 2 * x - 3 + (x + 1) * sqrt(pow(x, 2) - 9)) / (pow(x, 2) - 2 * x - 3 + (x - 1) * sqrt(pow(x, 2) - 9));
 
 	z2 = sqrt((x + 3) / (x - 3));
 
 	cout << endl;
-	//cout << "z1 = " << z1 << endl;
+	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
 
 	cin.get();
